718f9d555878bf9636b6c7a792d1fee4a184a65b
g275691 <g275691@yandex.ru>
2020-11-19 22:08:53 +0500

    Fixed teamblock size

--- module19_homework/app.js
+++ module19_homework/app.js
@@ -5,13 +5,13 @@ mobileBurger.addEventListener("click", function () {
     if(mobileBurger.classList.contains('is-active')) {
         document.querySelectorAll(".mobile__burger-rect").forEach((el,i) => {
         el.style.animation = `rect-${i} 1s ease-in-out both`; })
-        mobileBurger.classList.remove('is-active');
-        mobileMenu.classList.remove("is-active");
+        mobileBurger.classList.toggle('is-active');
+        mobileMenu.classList.toggle("is-active");
     } else {
         document.querySelectorAll(".mobile__burger-rect").forEach((el,i) => {
         el.style.animation = `rect-${i+3} 0.8s ease-out both reverse`; })
-        mobileBurger.classList.add('is-active');
-        mobileMenu.classList.add("is-active");
+        mobileBurger.classList.toggle('is-active');
+        mobileMenu.classList.toggle("is-active");
     }
 })
 
@@ -30,7 +30,5 @@ document.querySelectorAll(".questions__container-rect").forEach((el,i) => {
         document.querySelectorAll(".questions__container-answer")[i].classList.toggle("is-active");
     })
 });
-
-activateFunc = (query) => query.classList.toggle("is-active");
 
 
