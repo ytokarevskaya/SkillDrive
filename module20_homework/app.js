41c749d631b3dd4365a7c3e70069e14263a3d419
g275691 <g275691@yandex.ru>
2020-11-25 18:15:07 +0500

    add rent-rule block

--- module20_homework/app.js
+++ module20_homework/app.js
@@ -7,11 +7,18 @@ mobileBurger.addEventListener("click", function () {
         el.style.animation = `rect-${i} 1s ease-in-out both`; })
         mobileBurger.classList.toggle('is-active');
         mobileMenu.classList.toggle("is-active");
+        //Воздействие на элементы с позиционированием absolute
+        document.querySelectorAll(".ignore_menu").forEach(el => el.style.opacity = 0.05);
+        document.querySelectorAll(".ignore_menu").forEach(el => el.style.pointerEvents = "none");
     } else {
         document.querySelectorAll(".mobile__burger-rect").forEach((el,i) => {
         el.style.animation = `rect-${i+3} 0.8s ease-out both reverse`; })
         mobileBurger.classList.toggle('is-active');
         mobileMenu.classList.toggle("is-active");
+
+        document.querySelectorAll(".ignore_menu").forEach(el => el.style.opacity = 1);
+        document.querySelectorAll(".ignore_menu").forEach(el => el.style.pointerEvents = "all");
+
     }
 })
 
