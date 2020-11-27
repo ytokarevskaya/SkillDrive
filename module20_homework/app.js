505cbffe4d61956175f29a0cf3ac66dce66af5a7
g275691 <g275691@yandex.ru>
2020-11-27 13:50:24 +0500

    Add Module20

--- module20_homework/app.js
+++ module20_homework/app.js
@@ -7,18 +7,11 @@ mobileBurger.addEventListener("click", function () {
         el.style.animation = `rect-${i} 1s ease-in-out both`; })
         mobileBurger.classList.toggle('is-active');
         mobileMenu.classList.toggle("is-active");
-        //Воздействие на элементы с позиционированием absolute
-        document.querySelectorAll(".ignore_menu").forEach(el => el.style.opacity = 0.05);
-        document.querySelectorAll(".ignore_menu").forEach(el => el.style.pointerEvents = "none");
     } else {
         document.querySelectorAll(".mobile__burger-rect").forEach((el,i) => {
         el.style.animation = `rect-${i+3} 0.8s ease-out both reverse`; })
         mobileBurger.classList.toggle('is-active');
         mobileMenu.classList.toggle("is-active");
-
-        document.querySelectorAll(".ignore_menu").forEach(el => el.style.opacity = 1);
-        document.querySelectorAll(".ignore_menu").forEach(el => el.style.pointerEvents = "all");
-
     }
 })
 
@@ -36,45 +29,4 @@ document.querySelectorAll(".questions__container-rect").forEach((el,i) => {
         document.querySelectorAll(".questions__container-answer")[i].classList.toggle("is-active");
     })
 });
-
-/* review in Main-page */
-
-let slideShowCount = 1;
-let arrReviewImg = document.querySelector(".review__container").querySelectorAll("img");
-
-document.querySelector(".review__container").querySelectorAll(".icon-arrow")[0]
-.addEventListener("click", function () {
-    slideShowCount > 0 ? slideShowCount -= 1 : slideShowCount = 3;
-    document.querySelector(".review__container-slide__show-rect").innerHTML =
-    `<div class="wrapper">
-    <img src="${reviewImg[slideShowCount]}" alt="Client's portrait">
-    <div class="review__container-slide__show-rect-text">
-        <span class="review__container-slide__show-rect-text-name">${reviewName[slideShowCount]}</span>
-        <span class="review__container-slide__show-rect-text-city">${reviewCity[slideShowCount]}</span>
-    </div>
-</div>
-<span class="review__container-slide__show-rect-text-monolog">${reviewMonolog[slideShowCount]}</span>`
-})
-
-document.querySelector(".review__container").querySelectorAll(".icon-arrow")[1]
-.addEventListener("click", function () {
-    slideShowCount < 3 ? slideShowCount += 1 : slideShowCount = 0;
-    arrReviewImg.forEach(el => { el.classList.remove("is-active"); });
-    arrReviewImg[slideShowCount].classList.add("is-active");
-})
-
-document.querySelectorAll(".review__container-circles-circle").forEach((el,i) => {
-    el.addEventListener("click", function () {
-        slideShowCount = i;
-        document.querySelector(".review__container-slide__show-rect").innerHTML =
-        `<div class="wrapper">
-        <img src="${reviewImg[i]}" alt="Client's portrait">
-        <div class="review__container-slide__show-rect-text">
-            <span class="review__container-slide__show-rect-text-name">${reviewName[i]}</span>
-            <span class="review__container-slide__show-rect-text-city">${reviewCity[i]}</span>
-        </div>
-    </div>
-    <span class="review__container-slide__show-rect-text-monolog">${reviewMonolog[i]}</span>`
-    })
-})
 
