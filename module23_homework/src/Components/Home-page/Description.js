6ec7b4e0eaa0851be8a2ed7c7d4e46c5c4f42690
g275691 <g275691@yandex.ru>
2020-12-14 16:59:01 +0500

    add 2block-description

--- module23_homework/src/Components/Home-page/Description.js
+++ module23_homework/src/Components/Home-page/Description.js
@@ -3,33 +3,30 @@ import car2 from '../../img/home-page/car2.svg';
 import car3 from '../../img/home-page/car3.svg';
 import car4 from '../../img/home-page/car4.svg';
 
-const Description = () => (
+const Description = () => {
+    let DescriptionBlock = ({h, img, alt, span}) => (
+        <div class="main-description__container-block">
+        <img src={ img } alt={ alt } />
+        <div class="main-description__container-block-text">
+            <h2>{ h }</h2>
+            <span>{ span }</span>
+        </div>
+    </div>
+    )
+
+    return (
     <section class="main-description">
         <div class="main-description__container">
-            <div class="main-description__container-block">
-                <img src={ car2 } alt="" />
-                <div class="main-description__container-block-text">
-                    <h2>Аренда напрямую от владельцев</h2>
-                    <span>Вы получите автомобиль от его собственника, а мы проверим юридическую чистоту и техническую исправность.</span>
-                </div>
-            </div>
-            <div class="main-description__container-block">
-                <div class="main-description__container-block-text">
-                    <h2>Автомобили на любой вкус</h2>
-                    <span>Вы всегда можете подобрать автомобиль любого класса от бюджетных моделей до премиум-класса и спорткаров.</span>
-                </div>
-                <img src={ car3 } alt="" />
-            </div>
-            <div class="main-description__container-block">
-                <img src={ car4 } alt="" />
-                <div class="main-description__container-block-text">
-                    <h2>Гарантия честной аренды</h2>
-                    <span>Общение и оплата происходит через наш сервис, что предотвращает вас от обмана.</span>
-                </div>
-            </div>
+            <DescriptionBlock h="Аренда напрямую от владельцев" img={ car2 } alt = "car1" 
+            span="Вы получите автомобиль от его собственника, а мы проверим юридическую чистоту и техническую исправность." />
+            <DescriptionBlock h="Автомобили на любой вкус" img={ car3 } alt = "car2" 
+            span="Вы всегда можете подобрать автомобиль любого класса от бюджетных моделей до премиум-класса и спорткаров." />
+            <DescriptionBlock h="Аренда напрямую от владельцев" img={ car4 } alt = "car3" 
+            span="Общение и оплата происходит через наш сервис, что предотвращает вас от обмана." />
         </div>
     </section>
-)
+    )
+}
 
 export default Description;
 
