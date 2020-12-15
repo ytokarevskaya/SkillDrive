8553c64de1a80354199a2da04febfc36371938d9
g275691 <g275691@yandex.ru>
2020-12-15 12:08:17 +0500

    finish profit-block

--- module23_homework/src/index.js
+++ module23_homework/src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from 'react-router-dom';
-import './styles/glyphter.scss';
-import './styles/font-rule.scss';
-import "./styles/breakpoints.scss";
+
 import './styles/base.scss';
 
 import App from "./Components/app";
